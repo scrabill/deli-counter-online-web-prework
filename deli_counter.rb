22e0ddbfb3506e623176b24eb6960b613daa1d87
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 00:23:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,8 @@ def take_a_number(katz_deli,name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  katz_deli.shift
 
   puts "Currently serving Ada."
 end
