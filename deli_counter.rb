c5ca436f1b6c9082a307e0eea146694b6929975c
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 02:31:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each.with_index do |name, index|
-      message << "#{index}: #{name}"
+      puts message << "#{index}: #{name}"
     end
   end
 end
