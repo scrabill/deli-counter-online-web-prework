34c708eb4621c05dbd2caf1dccb31f33c891fa62
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 01:47:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,8 @@
 # Write your code here.
 # katz_deli = ["Price","Hanna",]
 
-katz_deli == []
-
 def line(katz_deli)
+  katz_deli == []
   if katz_deli == []
     puts "The line is currently empty."
   else
