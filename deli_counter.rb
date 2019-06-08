406bdcf13e196620e4a88f7a4dadfc4c5db2cf17
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 00:32:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,19 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  x = katz_deli.shift
-
-  puts "Currently serving #{x}."
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    x = katz_deli.shift
+    puts "Currently serving #{x}"
+  end
 end
+
+# 2.5.1 :107 > def serving(katz_deli)
+# 2.5.1 :108?>   if katz_deli.length == 0
+# 2.5.1 :109?>     puts "Closed"
+# 2.5.1 :110?>     else
+# 2.5.1 :111?>     x = katz_deli.shift
+# 2.5.1 :112?>     puts "Currently serving #{x}"
+# 2.5.1 :113?>     end
+# 2.5.1 :114?>   end
